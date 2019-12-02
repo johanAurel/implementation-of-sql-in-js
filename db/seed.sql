1d5632ac1be631d3892f1e27884859996f906a26
Tom Simmons <tomsimmons@Toms-MacBook-Pro-2.local>
2019-12-02 16:31:25 +0000

    simplify data

--- db/seed.sql
+++ db/seed.sql
@@ -5,7 +5,7 @@ CREATE DATABASE harry_potter;
 
 CREATE TABLE houses (
   house_id SERIAL PRIMARY KEY,
-  house_name VARCHAR(40),
+  house_name VARCHAR(40) NOT NULL,
   founder VARCHAR(40),
   animal VARCHAR(40)
 );
@@ -15,17 +15,6 @@ CREATE TABLE students (
   student_name VARCHAR(40),
   house_id INT,
   FOREIGN KEY (house_id) REFERENCES houses(house_id)
-);
-
-CREATE TABLE classes (
-  class_id SERIAL PRIMARY KEY,
-  class_name VARCHAR(40),
-  teacher VARCHAR(40)
-);
-
-CREATE TABLE spells (
-  spell_id SERIAL PRIMARY KEY,
-  spell_name VARCHAR(40)
 );
 
 INSERT INTO houses
@@ -54,28 +43,3 @@ VALUES
   ('Bram Bones', 3),
   ('Tomas Singh', 1),
   ('Matthew Krum', 2);
-
-INSERT INTO classes
-  (class_name, teacher)
-VALUES
-  ('FUN spells', 'Anat'),
-  ('BEND spells', 'Shaq'),
-  ('FEND spells', 'Alex'),
-  ('Project spells', 'Umbridge'),
-  ('Imposter Syndrome Spells', 'Haz');
-
-INSERT INTO spells
-  (spell_name)
-VALUES
-  ('Googleiarmus'),
-  ('Stack Overflowtia'),
-  ('Ask a Friendio'),
-  ('Nc Helpify'),
-  ('Array Methoducto'),
-  ('Destructurefy'),
-  ('Testingdio'),
-  ('Reactuendo'),
-  ('Promisify'),
-  ('Callbackio'),
-  ('Nodeiarmus'),
-  ('Api-ify');
