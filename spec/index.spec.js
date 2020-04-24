9bbf9365c7d30bebc6900c3605ce0613d8891b56
daviidbartlett <daviidbartlett@gmail.com>
2020-04-24 15:21:18 +0100

    swap out chai methods for jest

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,26 +1,27 @@
-const { expect } = require('chai');
 const app = require('../app');
 const request = require('supertest');
 const connection = require('../db/client.js');
 
 describe('/api', () => {
-  after(() => connection.end());
+  afterAll(() => connection.end());
   describe('/houses', () => {
-    it('GET:200 sends an array of houses to the client', () => {
+    test('GET:200 sends an array of houses to the client', () => {
       return request(app)
         .get('/api/houses')
         .expect(200)
         .then((response) => {
-          expect(response.body.houses).to.be.an('array');
-          expect(response.body.houses[0]).to.have.keys([
-            'house_id',
-            'house_name',
-            'founder',
-            'animal',
-          ]);
+          expect(response.body.houses).toEqual(expect.any(Array));
+          expect(Object.keys(response.body.houses[0])).toEqual(
+            expect.arrayContaining([
+              'house_id',
+              'house_name',
+              'founder',
+              'animal',
+            ])
+          );
         });
     });
-    it('POST:201 inserts a new house to the db and sends the new house back to the client', () => {
+    test('POST:201 inserts a new house to the db and sends the new house back to the client', () => {
       return request(app)
         .post('/api/houses')
         .send({
@@ -30,16 +31,18 @@ describe('/api', () => {
         })
         .expect(201)
         .then((response) => {
-          expect(response.body.house).to.be.an('object');
-          expect(response.body.house).to.have.keys([
-            'house_id',
-            'house_name',
-            'animal',
-            'founder',
-          ]);
+          expect(response.body.house).toEqual(expect.any(Object));
+          expect(Object.keys(response.body.house)).toEqual(
+            expect.arrayContaining([
+              'house_id',
+              'house_name',
+              'founder',
+              'animal',
+            ])
+          );
         });
     });
-    it('POST:400 responds with an appropriate error message when provided with a bad house (no name)', () => {
+    test('POST:400 responds with an appropriate error message when provided with a bad house (no name)', () => {
       return request(app)
         .post('/api/houses')
         .send({
@@ -48,87 +51,87 @@ describe('/api', () => {
         })
         .expect(400)
         .then((response) => {
-          expect(response.body.msg).to.equal('Bad request');
+          expect(response.body.msg).toBe('Bad request');
         });
     });
     describe('/:house_id', () => {
-      it('GET:200 sends a single house to the client', () => {
+      test('GET:200 sends a single house to the client', () => {
         return request(app)
           .get('/api/houses/1')
           .expect(200)
           .then((response) => {
-            expect(response.body.house).to.be.an('object');
-            expect(response.body.house.house_name).to.equal('Anatfindor');
+            expect(response.body.house).toEqual(expect.any(Object));
+            expect(response.body.house.house_name).toBe('Anatfindor');
           });
       });
-      it('GET:404 sends an appropriate and error message when given a valid but non-existent id', () => {
+      test('GET:404 sends an appropriate and error message when given a valid but non-existent id', () => {
         return request(app)
           .get('/api/houses/999')
           .expect(404)
           .then((response) => {
-            expect(response.body.msg).to.equal('House does not exist');
+            expect(response.body.msg).toBe('House does not exist');
           });
       });
-      it('GET:400 sends an appropriate and error message when given an invalid id', () => {
+      test('GET:400 sends an appropriate and error message when given an invalid id', () => {
         return request(app)
           .get('/api/houses/not-a-house')
           .expect(400)
           .then((response) => {
-            expect(response.body.msg).to.equal('Invalid id');
+            expect(response.body.msg).toBe('Invalid id');
           });
       });
-      it('DELETE:204 deletes the specified house and sends no body back', () => {
-        return request(app)
-          .delete('/api/houses/3')
-          .expect(204);
+      test('DELETE:204 deletes the specified house and sends no body back', () => {
+        return request(app).delete('/api/houses/3').expect(204);
         // No "then" because a 204 status responds with no body no matter what
       });
-      it('DELETE:404 responds with an appropriate error message when given a non-existent id', () => {
+      test('DELETE:404 responds with an appropriate error message when given a non-existent id', () => {
         return request(app)
           .delete('/api/houses/999')
           .expect(404)
           .then((response) => {
-            expect(response.body.msg).to.equal('House does not exist');
+            expect(response.body.msg).toBe('House does not exist');
           });
       });
       describe('/students', () => {
-        it('GET:200 sends an array of students belonging to a single house to the client', () => {
+        test('GET:200 sends an array of students belonging to a single house to the client', () => {
           return request(app)
             .get('/api/houses/1/students')
             .expect(200)
             .then((response) => {
-              expect(response.body.students).to.be.an('array');
-              expect(response.body.students[0]).to.have.keys([
-                'student_id',
-                'student_name',
-                'house_id',
-              ]);
+              expect(response.body.students).toEqual(expect.any(Array));
+              expect(Object.keys(response.body.students[0])).toEqual(
+                expect.arrayContaining([
+                  'student_id',
+                  'student_name',
+                  'house_id',
+                ])
+              );
               response.body.students.forEach((student) => {
-                expect(student.house_id).to.equal(1);
+                expect(student.house_id).toBe(1);
               });
             });
         });
-        it('GET:200 sends an empty array of students when there are no students in the house', () => {
+        test('GET:200 sends an empty array of students when there are no students in the house', () => {
           return request(app)
             .get('/api/houses/4/students')
             .expect(200)
             .then((response) => {
-              expect(response.body.students).to.eql([]);
+              expect(response.body.students).toEqual([]);
             });
         });
-        it('GET:404 sends an appropriate and error message when given a valid but non-existent id', () => {
+        test('GET:404 sends an appropriate and error message when given a valid but non-existent id', () => {
           return request(app)
             .get('/api/houses/999/students')
             .expect(404)
             .then((response) => {
-              expect(response.body.msg).to.equal('House does not exist');
+              expect(response.body.msg).toBe('House does not exist');
             });
         });
       });
     });
   });
   describe('/students', () => {
-    // it("GET:200 sends an array of all students to the client", () => {
+    // test("GET:200 sends an array of all students to the client", () => {
     //    Your turn...
     // });
   });
